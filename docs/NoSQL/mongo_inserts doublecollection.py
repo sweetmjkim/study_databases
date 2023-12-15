b9bc50dfe39d5ed6bee82c7a0a06e933efb7b8d9
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-15 17:10:49 +0900

    2023.12.15

--- docs/NoSQL/mongo_inserts doublecollection.py
+++ docs/NoSQL/mongo_inserts doublecollection.py
@@ -27,3 +27,43 @@ mixed_fruit = {"name": "오렌지",
                "origin": "미국"}
 result = collection.insert_one(mixed_fruit)
 pass
+
+# 분리 입력(fruits, fruits_colors)
+# insert fruits 작업 진행
+dict_fruit = {"name": "오렌지",
+               "origin": "미국"}
+result = collection.insert_one(dict_fruit)
+# insertedId: ObjectId("657bf1307880e42cec1aeedc")
+print("result.inserted_id : {}".format(result.inserted_id))
+inserted_id = result.inserted_id
+
+
+
+#insert fruits_colors 작업 진행
+# [{"fruits_id" : ObjectId("657bf1307880e42cec1aeedc"), "color": "주황색"}
+#  , {"fruits_id" : ObjectId("657bf1307880e42cec1aeedc"),"color": "갈색"}
+#  , {"fruits_id" : ObjectId("657bf1307880e42cec1aeedc"),"color": "노란색"}]
+
+
+# 아래 내용은 mongoDB를 통해서 진행했던걸 파이썬으로 만들어내는 과정이다.
+
+fruit_colors = [{"color": "주황색"}
+            , {"color": "갈색"}
+            , {"color": "노란색"}]
+
+list_fruits_colors = list()
+for dict_color in fruit_colors :
+    dict_color["fruits_id"] = inserted_id
+    list_fruits_colors.append(dict_color)
+    pass
+
+# collection fruits colors
+collection_fruits_colors = database["fruits_colors"]
+
+collection_fruits_colors.insert_many(list_fruits_colors)
+
+# find from fruits_colors
+documents = collection_fruits_colors.find({ "fruits_id" : { "$eq" : inserted_id}})
+
+pass
+# db.fruits_colors.find({ fruits_id : { $eq : ObjectId("657bf1307880e42cec1aeedc")}}) ;
