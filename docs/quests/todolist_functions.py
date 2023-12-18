399ffb0b77a6010f8642b3904a8341f9d69460c1
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-18 15:03:09 +0900

    ing

--- docs/quests/todolist_functions.py
+++ docs/quests/todolist_functions.py
@@ -12,7 +12,43 @@ def connect() :
     mongoClient = MongoClient("mongodb://localhost:27017")
     database = mongoClient["local"]
     collection = database["todolist"]
-    return collection
+    return todo_list
 
-def insert() :
+def insert(collection,todo_list) :
     collection.insert_many(todo_list)
+    
+insert(connect(),todo_list)
+
+
+# Input Your Name:  참여자1
+
+# ToDo List 중 하나 선택 하세요 !
+# 1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5.팀 멤버와의 1:1 면담
+# Title 번호 :  1
+# Status:  완료
+# 종료 여부 : c
+
+# ToDo List 중 하나 선택 하세요 !
+# 1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5.팀 멤버와의 1:1 면담
+# Title 번호 :  2
+# Status:  진행 중
+# 종료 여부 : q
+
+# ------------------------
+# Input Your Name:  참여자2
+# 1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5. 팀 멤버와의 1:1 면담
+# Title 번호 :  3
+# Status:  진행 중
+# 종료 여부 : q
+
+# ------------------------
+# Input Your Name:  참여자3
+
+# 1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5. 팀 멤버와의 1:1 면담
+# Title 번호 :  5
+# Status:  진행 중
+# 종료 여부 : x
+
+# ------------------------
+# 프로그램이 종료되었습니다.
+
